643794e264f9de2032970c67200447dbb8480b81
BluiWulf <bluiwulf@gmail.com>
2025-02-19 00:48:04 -0500

    adding player.py and adding ability to draw player to screen

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import pygame
 
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     clk = pygame.time.Clock()
     dt = 0
@@ -15,6 +18,7 @@ def main():
                 return
 
         screen.fill("black", rect=None, special_flags=0)
+        player.draw(screen)
         pygame.display.flip()
 
         dt = clk.tick(60) / 1000
